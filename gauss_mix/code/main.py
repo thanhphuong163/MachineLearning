25c4bfb4651c741386403c6343550d2c03978d99
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-10-21 11:38:32 +0700

    add guass_mix

---
+++ gauss_mix/code/main.py
@@ -0,0 +1,6 @@
+import numpy as np
+from utils.functions import my_square
+
+if __name__ == "__main__":
+    n = 3
+    print(my_square(n))
---
+++ gauss_mix/code/utils/functions.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def my_square(n):
+    return n*n
