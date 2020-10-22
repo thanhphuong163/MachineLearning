ed0d43e0250f3e4e4094d7127e6b1c1e9523e670
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-10-22 16:06:33 +0700

    add k-mean algorithm

--- gauss_mix/code/main.py
+++ gauss_mix/code/main.py
@@ -1,6 +1,45 @@
 import numpy as np
-from utils.functions import my_square
+import pandas as pd
+import matplotlib.pyplot as plt
+from models.k_mean import k_mean
+
+
+def genGauss(n=100, loc=np.array([0, 0]), scale=np.array([1, 1])):
+    dim = loc.shape[0]
+    X = np.random.normal(loc=loc, scale=scale, size=(n, dim))
+    return X
+
+
+def genGaussMix(n=100, p=np.array([.5, .5]), loc=np.array([[-2, 0], [2, 0]]), scale=np.array([[1, 1], [1, 1]])):
+    k = loc.shape[0]
+    lstX = []
+    for i in range(k):
+        samples = int(round(p[i]*n))
+        X = genGauss(n=samples, loc=loc[i, :], scale=scale[i, :])
+        Y = np.empty((samples, 1))
+        Y.fill(i+1)
+        lstX.append(np.concatenate((X, Y), axis=1))
+    return np.concatenate(lstX, axis=0)
+
+
+def plotGaussMix(X):
+    labels = np.unique(X[:, -1])
+    plt.figure(figsize=(10, 7))
+    for i in labels:
+        data = X[X[:,-1] == i]
+        plt.scatter(data[:, 0], data[:, 1], label=f'Cluster {int(i)}')
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
-    n = 3
-    print(my_square(n))
+    n = 300
+    p = np.array([.5, .3, .2])
+    loc = np.array([[-2, -1], [2, 5], [3, -2]])
+    scale = np.array([[2, 2], [1, 1], [1, 3]])
+    X = genGaussMix(n=n, p=p, loc=loc, scale=scale)
+    # plotGaussMix(X)
+    kmean = k_mean.Kmean(num_clusters=3, num_iters=4)
+    data, loss = kmean(X[:,:-1])
+    # print(data)
+    print(loss)
+    plotGaussMix(data)
---
+++ gauss_mix/code/models/k_mean/k_mean.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+class Kmean(object):
+    def __init__(self, num_clusters=2, num_iters=10):
+        self.num_clusters = num_clusters
+        self.num_iters = num_iters
+
+    @staticmethod
+    def calc_distance(x1, x2):
+        '''
+        Calculate Euclide's distance between two points
+        '''
+        return np.sqrt(np.sum((x1-x2)**2))
+
+    @staticmethod
+    def assign_cluster(X:np.array, clusters:np.array):
+        '''
+        The point x will be assigned to the closest cluster
+        '''
+        for i, x in enumerate(X):
+            X[i,-1] = np.argmax(np.sum((x[:-1].reshape((1, x.shape[0]-1)) - clusters)**2, axis=1)) + 1
+        return X
+    
+    @staticmethod
+    def update_cluster(X):
+        labels = np.unique(X[:, -1])
+        means = list()
+        for label in labels:
+            data = X[X[:, -1] == label]
+            means.append(data[:, :-1].mean(axis=0))
+        return np.array(means)
+
+    @staticmethod
+    def loss(X, means):
+        cost = 0
+        for i, mean in enumerate(means):
+            data = X[X[:, -1] == i+1][:, :-1]
+            cost += np.sum((data-mean)**2)
+        return np.sqrt(cost) / X.shape[0]
+
+    def __call__(self, X:np.array):
+        means = X[np.random.choice(X.shape[0], self.num_clusters, replace=False), :]
+        lst_loss = list()
+        labels = np.zeros((X.shape[0], 1))
+        X = np.concatenate((X, labels), axis=1)
+        for i in range(self.num_iters):
+            X = self.assign_cluster(X, means)
+            lst_loss.append(self.loss(X, means))
+            means = self.update_cluster(X)
+        return X, lst_loss
